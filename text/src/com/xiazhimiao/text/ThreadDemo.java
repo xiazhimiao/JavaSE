da4c86e91590989a67fb893ac33b6acd50047eda
xiazhimiao <3309686397@qq.com>
2024-02-24 20:12:23 +0800

    Java虚拟机可用线程数量

---
+++ text/src/com/xiazhimiao/text/ThreadDemo.java
@@ -0,0 +1,9 @@
+package com.xiazhimiao.text;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        //获取Java虚拟机能使用的线程
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        //20
+    }
+}
